7770b42f2759e400b98b9709ab0335a019cd67ae
pyae-bhone-khant <pyaebhonen4@gmail.com>
2026-06-02 17:23:09 +0630

    fix: resolve correct relative path for config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from "./app.js";
-import config from "../src/config/index.js";
+import config from "./config/index.js";
 
 app.listen(config.port, () => {
   console.log(`Express server listening on port ${config.port}`);
