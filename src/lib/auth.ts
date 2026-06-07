285eea5cb1aa8f90d87c93a068395dc60e1421f5
pyae-bhone-khant <pyaebhonen4@gmail.com>
2026-06-07 19:30:41 +0630

    test for deply auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -115,5 +115,5 @@ export const auth = betterAuth({
       },
     },
   },
-  trustedOrigins: ["https://meeting-room-booking-system-neon.vercel.app", "https://meeting-room-booking-system-q6xr0ifk6.vercel.app" , "http://localhost:3000"],
+  trustedOrigins: ["https://meeting-room-booking-system-neon.vercel.app", "https://meeting-room-booking-system-q6xr0ifk6.vercel.app" , "http://localhost:3000" , "https://meeting-room-booking-system-iota.vercel.app"],
 });
